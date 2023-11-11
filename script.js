18d147756c9a5f1f4c7b9fea0fc17c0dbe9ff59a
AshRossel <ashrossel@gmail.com>
2023-11-11 18:41:45 +0000

    Add checking functions

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const add = (number1, number2) => number1 + number2;
 const subtract = (number1, number2) => number1 - number2;
 const multiply = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => (number2 == 0) ? 0 : number1 / number2;
+const isNumber = (number) => !isNaN(number) ? true : false;
+const hasPoint = (number) => (number.indexOf('.') != -1) ? true : false;
