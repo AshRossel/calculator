358e306fff265a6033e1813bde9d9255204b3a95
AshRossel <ashrossel@gmail.com>
2023-11-11 22:35:19 +0000

    Add display character removal function

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const subtract = (number1, number2) => number1 - number2;
 const multiply = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => (number2 == 0) ? 0 : number1 / number2;
 const isNumber = (number) => !isNaN(number) ? true : false;
-const changeOperator = (newOperator) => display.textContent.slice(0, -1) + newOperator;
 const hasPoint = (number) => (number.indexOf('.') != -1) ? true : false;
+const changeOperator = (newOperator) => display.textContent.slice(0, -1) + newOperator;
+const removeDisplayChar = (charNumbers) => display.textContent.slice(0, -charNumbers);
 
 const hasDecimalNumber = function(number) {
     return (number != undefined && number % 1 !== 0) ? true : false;
