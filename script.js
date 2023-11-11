e49e1415df097eed51dc61f9aa72e663ee804e5c
AshRossel <ashrossel@gmail.com>
2023-11-11 18:46:21 +0000

    Add checking decimal number function

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const multiply = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => (number2 == 0) ? 0 : number1 / number2;
 const isNumber = (number) => !isNaN(number) ? true : false;
 const hasPoint = (number) => (number.indexOf('.') != -1) ? true : false;
+
+const hasDecimalNumber = function(number) {
+    return (number != undefined && number % 1 !== 0) ? true : false;
+}
