5a34f1334d76c3458345a169b54c10f501d94da4
AshRossel <ashrossel@gmail.com>
2023-11-11 22:33:34 +0000

    Add change operator function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const subtract = (number1, number2) => number1 - number2;
 const multiply = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => (number2 == 0) ? 0 : number1 / number2;
 const isNumber = (number) => !isNaN(number) ? true : false;
+const changeOperator = (newOperator) => display.textContent.slice(0, -1) + newOperator;
 const hasPoint = (number) => (number.indexOf('.') != -1) ? true : false;
 
 const hasDecimalNumber = function(number) {
